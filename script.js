5bd91239d6bff485cb165e223f20cf211a76fdc0
Evgeni.Marinov <marinov.evgeny@gmail.com>
2020-04-08 20:28:42 +0300

    New FormValidator

---
+++ script.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show the error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show the success line
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Capitalize the first character
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check all requireds
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+// Check length of input
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check if email is corect
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim().toLowerCase())) {
+        showSuccess(input);
+    } else {
+        showError(input, `${getFieldName(input)} is not valid`);
+    }
+}
+
+// Check if passwords are same
+function comparePassword(input, input2) {
+    if (input.value !== input2.value || input2.value === '') {
+        showError(input2, `Password do not match`);
+    } else {}
+}
+
+// Event listener
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 16);
+    checkLength(password, 6, 16);
+    checkEmail(email);
+    comparePassword(password, password2);
+});
